77857b608f6c67d4b1313905fe02eafc89cc1894
sunnyWu1104 <wujiapengxp1104@gmail.com>
2018-07-16 16:31:47 +0800

    add AsyncRoot

--- app/src/main/java/com/example/springboot2docker/controller/IDController.java
+++ app/src/main/java/com/example/springboot2docker/controller/IDController.java
@@ -32,6 +32,41 @@ public class IDController {
     @RequestMapping("/id")
     public void getId(){
         logger.info(JSONObject.toJSONString(idService.genId()));
+        logger.trace(JSONObject.toJSONString(idService.genId()));
+    }
+
+    @RequestMapping("/asyncId")
+    public void asyncId(){
+        Thread t1 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                long l1 = System.currentTimeMillis();
+                for(int i=0;i<1000000;i++){
+
+                    logger.info("线程1,"+JSONObject.toJSONString(idService.genId()));
+                }
+                long l2 = System.currentTimeMillis();
+                System.err.println("线程1："+(l2-l1));
+            }
+        });
+
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long l1 = System.currentTimeMillis();
+                for(int i=0;i<1000000;i++){
+                    logger.info("线程2,"+JSONObject.toJSONString(idService.genId()));
+                }
+                long l2 = System.currentTimeMillis();
+                System.err.println("线程2："+(l2-l1));
+            }
+        });
+
+        t1.start();
+        t2.start();
+
     }
 
 }
