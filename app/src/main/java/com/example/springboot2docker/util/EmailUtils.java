aaa8c1d31341bd9951da60096cdf4b700b1b18fd
sunnyWu1104 <wujiapengxp1104@gmail.com>
2018-11-26 14:35:16 +0800

    add email util

---
+++ app/src/main/java/com/example/springboot2docker/util/EmailUtils.java
@@ -0,0 +1,103 @@
+package com.example.springboot2docker.util;
+
+
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
+import java.util.Properties;
+
+public final class EmailUtils {
+    /**
+     * 公司统一邮箱
+     */
+    private static String emailAccount = "1786948131@qq.com";
+    /**
+     * 邮箱密码
+     */
+    private static String emailPassWord = "123123123";
+    /**
+     * 邮箱的支持的smtp 服务地址
+     */
+    private static String SMTPHost = "smtp.exmail.qq.com";
+    /**
+     * smtp的端口号
+     */
+    private static String SMTPPort = "465";
+
+
+    public static void sendSimpleMail(String recipient, String subject, Object content) throws Exception {
+        // 1. 创建参数配置, 用于连接邮件服务器的参数配置
+        Properties props = new Properties();                    // 参数配置
+        props.setProperty("mail.transport.protocol", "smtp");   // 使用的协议（JavaMail规范要求）
+        props.setProperty("mail.smtp.host", SMTPHost);   // 发件人的邮箱的 SMTP 服务器地址
+        props.setProperty("mail.smtp.auth", "true");            // 需要请求认证
+
+        // 某些邮箱服务器要求 SMTP 连接需要使用 SSL 安全认证 (为了提高安全性, 邮箱支持SSL连接, 也可以自己开启),
+        //     如果无法连接邮件服务器, 仔细查看控制台打印的 log, 如果有有类似 “连接失败, 要求 SSL 安全连接” 等错误,
+        //     打开下面 /* ... */ 之间的注释代码, 开启 SSL 安全连接。
+        // SMTP 服务器的端口 (非 SSL 连接的端口一般默认为 25, 可以不添加, 如果开启了 SSL 连接,
+        //                  需要改为对应邮箱的 SMTP 服务器的端口, 具体可查看对应邮箱服务的帮助,
+        //                  QQ邮箱的SMTP(SLL)端口为465或587, 其他邮箱自行去查看)
+
+        props.setProperty("mail.smtp.port", SMTPPort);
+        props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.setProperty("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.socketFactory.port", SMTPPort);
+        // 2. 根据配置创建会话对象, 用于和邮件服务器交互
+        Session session = Session.getDefaultInstance(props);
+        session.setDebug(true);
+
+        // 3. 创建一封邮件
+        MimeMessage message = createMimeMessage(session, emailAccount, recipient, subject, content);
+
+        // 4. 根据 Session 获取邮件传输对象
+        Transport transport = session.getTransport();
+
+        // 5. 使用 邮箱账号 和 密码 连接邮件服务器
+        //    这里认证的邮箱必须与 message 中的发件人邮箱一致，否则报错
+        transport.connect(emailAccount, emailPassWord);
+
+        // 6. 发送邮件, 发到所有的收件地址, message.getAllRecipients() 获取到的是在创建邮件对象时添加的所有收件人, 抄送人, 密送人
+        transport.sendMessage(message, message.getAllRecipients());
+
+        // 7. 关闭连接
+        transport.close();
+    }
+
+    public static MimeMessage createMimeMessage(Session session, String sendMail, String receiveMail, String subject, Object content) throws Exception {
+        // 1. 创建一封邮件
+        MimeMessage message = new MimeMessage(session);
+
+        // 2. From: 发件人
+        message.setFrom(new InternetAddress(sendMail, "bigpong", "UTF-8"));
+
+        // 3. To: 收件人（可以增加多个收件人、抄送、密送）
+        message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiveMail, "尊敬的用户", "UTF-8"));
+
+        // 4. Subject: 邮件主题
+        message.setSubject(subject, "UTF-8");
+
+        // 5. Content: 邮件正文（可以使用html标签）
+        message.setContent(content.toString(), "text/html;charset=UTF-8");
+
+        // 6. 设置发件时间
+        message.setSentDate(new Date());
+
+        // 7. 保存设置
+        message.saveChanges();
+
+        return message;
+    }
+    public static void main(String[] args) {
+        try {
+            EmailUtils.sendSimpleMail("1786948131@qq.com","主题是测试","测试");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
